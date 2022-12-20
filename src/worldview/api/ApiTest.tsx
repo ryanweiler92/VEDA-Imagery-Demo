391038d7df235af65e62173e8da99a3047a0148b
ryanweiler92 <ryanweiler92@gmail.com>
2022-12-20 15:04:52 -0500

    Testing Response Data Functionality

--- src/worldview/api/ApiTest.tsx
+++ src/worldview/api/ApiTest.tsx
@@ -2,7 +2,14 @@ import React, { useContext, useState, useRef, useEffect } from "react"
 import { Box, Button, Flex, IconButton, Heading, List, ListItem, Text, Icon, Spinner, Spacer, Select } from "@chakra-ui/react";
 import axios from "axios"
 import { PhoneIcon, InfoIcon } from '@chakra-ui/icons';
+import { GiSave, GiMaterialsScience } from 'react-icons/gi'
+import { GoGlobe } from 'react-icons/go'
 import { apiCalls } from "./apiConfig";
+import { 
+    testResponseOneData, 
+    testResponseTwoData,
+    testResponseThreeData
+} from './dataTestingFunctions';
 
 const ApiTest = () => {
     const [responseOne, setResponseOne] = useState();
@@ -65,7 +72,7 @@ const ApiTest = () => {
             console.log(responseOne)
         } else if(e.target.id == "button2"){
             console.log(responseTwo)
-        } else if(e.target.id == "button2"){
+        } else if(e.target.id == "button3"){
             console.log(responseThree)
         }
       }
@@ -87,6 +94,39 @@ const ApiTest = () => {
             setCallDescriptionThree(value.description)
         }
       }
+
+      const saveToLocalStorage = (e) => {
+        console.log(e.target)
+        if(e.target.id == "save1"){
+            localStorage.setItem('response1', JSON.stringify(responseOne));
+            console.log("Response 1 saved to local storage.")
+        } else if (e.target.id == "save2"){
+            localStorage.setItem('response2', JSON.stringify(responseTwo));
+            console.log("Response 2 saved to local storage.")
+        } else if (e.target.id == "save3") {
+            localStorage.setItem('response3', JSON.stringify(responseThree));
+            console.log("Response 3 saved to local storage.")
+        }
+      }
+
+      const fetchLocalStorage = (e) => {
+        if(e.target.id == "test1"){
+            const lsData = localStorage.getItem('response1');
+            if(!lsData){console.log('No data found in local storage for response 1.')}
+            const response = JSON.parse(lsData);
+            testResponseOneData(response);
+        } else if (e.target.id == "test2"){
+            const lsData = localStorage.getItem('response2');
+            if(!lsData){console.log('No data found in local storage for response 2.')}
+            const response = JSON.parse(lsData);
+            testResponseTwoData(response);
+        } else if (e.target.id == "test3") {
+            const lsData = localStorage.getItem('response3');
+            if(!lsData){console.log('No data found in local storage for response 3.')}
+            const response = JSON.parse(lsData);
+            testResponseThreeData(response);
+        }
+      }
         
     return (
         <>
@@ -99,7 +139,7 @@ const ApiTest = () => {
             isLoading={loadingOne}
             isDisabled={!callUrlOne}
             >
-            <PhoneIcon mr="2" /> Fetch
+                <PhoneIcon mr="2" /> Fetch
             </Button>
             <Button 
             aria-label="fetch" 
@@ -109,7 +149,7 @@ const ApiTest = () => {
             isLoading={loadingTwo}
             isDisabled={!callUrlTwo}
             >
-            <PhoneIcon mr="2" /> Fetch
+                <PhoneIcon mr="2" /> Fetch
             </Button>
             <Button 
             aria-label="fetch" 
@@ -119,12 +159,12 @@ const ApiTest = () => {
             isLoading={loadingThree}
             isDisabled={!callUrlThree}
             >
-            <PhoneIcon mr="2" /> Fetch
+                <PhoneIcon mr="2" /> Fetch
             </Button>
         </Flex>
         <Flex w="100%" bg="gray.100" h="300px"> 
             <Flex align="center" direction="column" h="100%" w="33%" border="2px solid black">
-                <Select defaultValue="disabled" id="select1" bg="teal.300" borderRadius="0" textAlign="center" onChange={(e) => handleSelect(e)}>
+                <Select defaultValue="disabled" id="select1" bg="teal.300" borderRadius="0" textAlign="center" fontWeight="bold" onChange={(e) => handleSelect(e)}>
                     <option value="disabled" disabled>Select an API Request</option>
                     {apiCalls.map((call) => {
                         return (
@@ -137,7 +177,7 @@ const ApiTest = () => {
                 <Text align="center" my="2">{callUrlOne}</Text>
             </Flex>
             <Flex align="center" direction="column" h="100%" w="33%" border="2px solid black">
-                <Select defaultValue="disabled" id="select2" bg="teal.300" borderRadius="0" textAlign="center" onChange={(e) => handleSelect(e)}>
+                <Select defaultValue="disabled" id="select2" bg="teal.300" borderRadius="0" textAlign="center" fontWeight="bold" onChange={(e) => handleSelect(e)}>
                     <option value="disabled" disabled>Select an API Request</option>
                     {apiCalls.map((call) => {
                         return (
@@ -150,7 +190,7 @@ const ApiTest = () => {
                 <Text align="center" my="2">{callUrlTwo}</Text>
             </Flex>
             <Flex align="center" direction="column" h="100%" w="34%" border="2px solid black">
-                <Select defaultValue="disabled" id="select3" bg="teal.300" borderRadius="0" textAlign="center" onChange={(e) => handleSelect(e)}>
+                <Select defaultValue="disabled" id="select3" bg="teal.300" borderRadius="0" textAlign="center" fontWeight="bold" onChange={(e) => handleSelect(e)}>
                     <option value="disabled" disabled>Select an API Request</option>
                     {apiCalls.map((call) => {
                         return (
@@ -164,29 +204,112 @@ const ApiTest = () => {
             </Flex>
         </Flex>
         <Flex justify="space-around" align="center" my="2">
+        <Flex>
+            <Flex direction="column">
+                <Button 
+                aria-label="fetch" 
+                colorScheme='teal'
+                size='lg'
+                onClick={(e) => consoleResponse(e)}
+                id="button1"
+                isLoading={loadingOne}
+                disabled={!responseOne}
+                mb="2"
+                >
+                    <InfoIcon mr="2"/> Console
+                </Button>
+                <Button
+                aria-label="save"
+                colorScheme='teal'
+                size='lg'
+                isDisabled={!responseOne}
+                id="save1"
+                onClick={(e) => saveToLocalStorage(e)}
+                isLoading={loadingOne}
+                >
+                    <GiSave /> Save
+                </Button>
+            </Flex>
+            <Flex direction="column" ml="1">
+                <Button 
+                    aria-label="test data" 
+                    colorScheme='teal'
+                    size='lg'
+                    onClick={(e) => fetchLocalStorage(e)}
+                    id="test1"
+                    isLoading={loadingOne}
+                    // disabled={!responseOne}
+                    mb="2"
+                    >
+                    <GiMaterialsScience /> Test Data
+                </Button>
+                <Button 
+                    aria-label="apply to map" 
+                    colorScheme='teal'
+                    size='lg'
+                    id="map1"
+                    isLoading={loadingOne}
+                    disabled={true}
+                    mb="2"
+                    >
+                    <GoGlobe /> Apply To Map
+                </Button>
+            </Flex>
+            </Flex>
+            <Flex>
+            <Flex direction="column">
             <Button 
-            aria-label="fetch" 
-            colorScheme='teal'
-            size='lg'
-            onClick={(e) => consoleResponse(e)}
-            id="button1"
-            isLoading={loadingOne}
-            disabled={!responseOne}
-            >
-                <InfoIcon mr="2"/> Console
-            </Button>
-
-            <Button 
-            aria-label="fetch" 
+            aria-label="console" 
             colorScheme='red'
             size='lg'
             onClick={(e) => consoleResponse(e)}
             id="button2"
             isLoading={loadingTwo}
             disabled={!responseTwo}
+            mb="2"
             >
                 <InfoIcon mr="2"/> Console
             </Button>
+            <Button
+                aria-label="save"
+                colorScheme='red'
+                size='lg'
+                disabled={!responseTwo}
+                id="save2"
+                onClick={(e) => saveToLocalStorage(e)}
+                isLoading={loadingTwo}
+                >
+                    <GiSave /> Save 
+                </Button>
+            </Flex>
+            <Flex direction="column" ml="1">
+                <Button 
+                    aria-label="test data" 
+                    colorScheme='red'
+                    size='lg'
+                    onClick={(e) => fetchLocalStorage(e)}
+                    id="test2"
+                    isLoading={loadingTwo}
+                    // disabled={!responseTwo}
+                    mb="2"
+                    >
+                    <GiMaterialsScience /> Test Data
+                </Button>
+                <Button 
+                    aria-label="apply to map" 
+                    colorScheme='red'
+                    size='lg'
+                    id="map2"
+                    isLoading={loadingTwo}
+                    disabled={true}
+                    mb="2"
+                    >
+                    <GoGlobe /> Apply To Map
+                </Button>
+                </Flex>
+            </Flex>
+            <Flex>
+            <Flex direction="column">
             <Button 
             aria-label="fetch" 
             colorScheme='blue'
@@ -195,9 +318,48 @@ const ApiTest = () => {
             id="button3"
             isLoading={loadingThree}
             disabled={!responseThree}
+            mb="2"
             >
                 <InfoIcon mr="2"/> Console
             </Button>
+            <Button
+                aria-label="save"
+                colorScheme='blue'
+                size='lg'
+                isDisabled={!responseThree}
+                id="save3"
+                onClick={(e) => saveToLocalStorage(e)}
+                isLoading={loadingThree}
+                >
+                    <GiSave /> Save
+                </Button>
+            </Flex>
+            <Flex direction="column" ml="1">
+                <Button 
+                    aria-label="test data" 
+                    colorScheme='blue'
+                    size='lg'
+                    onClick={(e) => fetchLocalStorage(e)}
+                    id="test3"
+                    isLoading={loadingThree}
+                    // disabled={!responseThree}
+                    mb="2"
+                    >
+                    <GiMaterialsScience /> Test Data
+                </Button>
+                <Button 
+                    aria-label="apply to map" 
+                    colorScheme='blue'
+                    size='lg'
+                    id="map3"
+                    isLoading={loadingThree}
+                    mb="2"
+                    disabled={true}
+                    >
+                    <GoGlobe /> Apply To Map
+                </Button>
+                </Flex>
+            </Flex>
         </Flex>
         </>
     )
--- src/worldview/api/apiConfig.ts
+++ src/worldview/api/apiConfig.ts
@@ -2,7 +2,17 @@ export const apiCalls = [
     {
         title: "List All Collections",
         url: "https://staging-stac.delta-backend.com/",
-        description: "Get a list of all the available collections. This includes HLSL30.002.",
+        description: "Get a list of all the available collections. This includes HLSL30.002. This is the STAC_API_URL in the HLS example in the VEDA Docs. links[19] == `HSL30.002 Environmental Justice Events`",
+    },
+    {
+        title: "Raster API",
+        url: "https://staging-raster.delta-backend.com/",
+        description: "Raster API. Returns nothing ATM."
+    },
+    {
+        title: "STAC API /search",
+        url: "https://staging-stac.delta-backend.com/search",
+        description: "STAC search. Returns feature collections."
     }
   ];
 
